f815fea18435ad2eff967fd8e280df847a4c052b
Alex S <Ascendor@users.noreply.github.com>
2023-03-29 21:44:09 +0200

    Update global_preferences.py

--- src/boinc_client/global_preferences.py
+++ src/boinc_client/global_preferences.py
@@ -2,11 +2,11 @@ import xmltodict
 
 from boinc_client.clients.rpc_client import RpcClient
 
-def set_global_prefs_override(client: RpcClient, mode: str = "") -> dict:    
+def set_global_prefs_override(client: RpcClient, pref: str = "") -> dict:    
     """Set global preference override."""
-    rpc_resp = client.make_request("<set_global_prefs_override><global_preferences><cpu_usage_limit>50</cpu_usage_limit></global_preferences></set_global_prefs_override>")
+    rpc_resp = client.make_request("<set_global_prefs_override><global_preferences>" + pref + "</global_preferences></set_global_prefs_override>")
     rpc_json = xmltodict.parse(rpc_resp)
-    read_global_prefs_override(client=client)
+    read_global_prefs_override(client=client)   #prefs have to be read to activate them
     return rpc_json
 
 def read_global_prefs_override(client: RpcClient) -> dict:    
